ca8346dc36f798b552363c81e0fa800138a107a1
daniBagaskara <bagaskaradani724@gmail.com>
2025-04-12 14:17:38 +0700

    refactor: simplify MongoDB connection logic and update environment variables

--- connection.js
+++ connection.js
@@ -1,17 +1,18 @@
 import dotenv from 'dotenv'
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const env = dotenv.config().parsed;
+const env = dotenv.config().parsed
 
 const connection = () => {
-    mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_USERNAME}:${env.MONGODB_PASSWORD}@${env.MONGODB_HOST}:${env.MONGODB_PORT}`, { 
-        dbName: `${env.MONGODB_DB_NAME}`,
-    })
-    const connection = mongoose.connection;
-    connection.on('error', console.error.bind(console, 'connection error:'));
-    connection.once('open', () => {
-        console.log('Connected to MongoDB');
-    });
+  mongoose.connect(env.MONGODB_URI, {
+    dbName: env.MONGODB_DB_NAME,
+  })
+
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', () => {
+    console.log('Connected to MongoDB')
+  })
 }
 
-export default connection;
+export default connection
